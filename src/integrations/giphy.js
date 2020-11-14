5ff869f8b9c95cc77f44cc831b3270a4b00777c1
vitorAlmeid <vitoraaalmeida@live.com>
2020-11-14 14:34:49 -0300

    add giphy integration

---
+++ src/integrations/giphy.js
@@ -0,0 +1,13 @@
+const Fetch = require('../lib/fetch').doFetch;
+const API_KEY = 'rcXN20npMs3UowQo9pe1QKGM9ctk51Ag';
+
+const BASE_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=1&q=`;
+
+const getGifs = async function getGifs(recipe) {
+
+    return Fetch('GET', BASE_URL, false, { params: recipe });
+};
+
+module.exports = {
+    getGifs
+}
