18b68e9ea0c5a7a0d73b4fa7c552f6e99b8f904b
vitorAlmeid <vitoraaalmeida@live.com>
2020-11-14 11:24:27 -0300

    add basic recipePuppy integration

--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -1,7 +1,47 @@
+'use strict';
+const recipePuppy = require('../integrations/recipePuppy');
+const validateQuery = query => {
+
+    if (!query.i) return { isValid: false, error: 'Invalid Query' };
+
+    if (query.i.split(',').length > 3) return { isValid: false, error: 'Cant use more than three ingredients' };
+
+    for (const item of query.i.split(',')) {
+        if (item.trim().length < 3) {
+            return { isValid: false, error: `Cant use this ingredient [${item}]` };
+        }
+    }
+
+    return { isValid: true };
+};
+
+const getRecipes = async function (ingredients) {
+    return new Promise( async (resolve, reject) => {
+
+        const [error, recipes] = await to(recipePuppy.getRecipes(ingredients));
+
+        if (error) {
+            return reject(error);
+        }
+
+        resolve(recipes);
+    });
+
+};
+
+
 module.exports = {
 
     async search(req, res) {
-        
-        res.status(200).json({ data: req.query });
+
+        const validation = validateQuery(req.query);
+
+        if (!validation.isValid) return res.status(400).json({ error: validation.error });
+
+        const recipes = await getRecipes(req.query.i.replace(' ', ''))
+
+        if (!recipes) return res.status(204).json({ error: 'Recipe puppy unavailable'  });
+
+        res.status(200).json({ data: recipes });
     }
 }
---
+++ src/integrations/recipePuppy.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Fetch = require('../lib/fetch').doFetch;
+const BASE_URL = 'http://www.recipepuppy.com/api/?i=';
+
+const getRecipes = async function getRecipes(ingredients) {
+
+    return Fetch('GET', BASE_URL, false, { params: ingredients });
+};
+
+module.exports = {
+    getRecipes
+}
